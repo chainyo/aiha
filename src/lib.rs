c1b6b233b42566a72b5a6cfa37e169e2ff61caf9
chainyo <thomas@deeptools.ai>
2023-04-24 00:02:12 +0200

    fix rust lib + tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,10 @@
-#[macro_use]
 pub mod scan;
+
+use pyo3::prelude::*;
+
+#[pymodule]
+fn aiha(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<scan::Scan>()?;
+    Ok(())
+}
+
--- src/scan/scan.rs
+++ src/scan/scan.rs
@@ -1,10 +1,15 @@
-use std::env::consts::OS;
-use std::env::consts::ARCH;
+use pyo3::prelude::*;
+use std::env::consts::{ARCH, OS};
 
-/// A struct that provides scanning functionality for the user hardware.
-pub struct Scan;
+#[pyclass]
+pub struct Scan {}
 
+#[pymethods]
 impl Scan {
+    #[new]
+    pub fn new() -> Self {
+        Scan {}
+    }
     /// Returns a string representing the operating system the code is running on.
     ///
     /// # Examples
@@ -14,21 +19,23 @@ impl Scan {
     /// let os = scanner.os_scan();
     /// println!("Operating system: {}", os);
     /// ```
-    pub fn os_scan(&self) -> String {
-        OS.to_string()
+    #[pyo3(text_signature = "($self)")]
+    pub fn os_scan(&self) -> PyResult<String> {
+        Ok(OS.to_string())
     }
 
     /// Returns a string representing the CPU architecture the code is running on.
-    /// 
+    ///
     /// # Examples
-    /// 
+    ///
     /// ```
     /// let scanner = Scanner;
     /// let arch = scanner.arch_scan();
     /// println!("CPU architecture: {}", arch);
     /// ```
-    pub fn arch_scan(&self) -> String {
-        ARCH.to_string()
+    #[pyo3(text_signature = "($self)")]
+    pub fn arch_scan(&self) -> PyResult<String> {
+        Ok(ARCH.to_string())
     }
 }
 
@@ -36,17 +43,32 @@ impl Scan {
 mod tests {
     use super::*;
 
+    // Test for the new() method of the Scan struct
     #[test]
-    fn test_os_scan() {
-        let scan = Scan;
-        let os = scan.os_scan();
-        assert_eq!(os, OS.to_string());
+    fn test_scan_new() {
+        let scan = Scan::new();
+        assert!(scan.os_scan().is_ok());
+        assert!(scan.arch_scan().is_ok());
     }
 
+    // Test for the os_scan() method of the Scan struct
     #[test]
-    fn test_arch_scan() {
-        let scan = Scan;
-        let arch = scan.arch_scan();
-        assert_eq!(arch, ARCH.to_string());
+    fn test_scan_os_scan() {
+        let scan = Scan::new();
+        let os_pyresult = scan.os_scan();
+        assert!(os_pyresult.is_ok());
+        let os_string = os_pyresult.unwrap();
+        assert_eq!(os_string, OS);
+    }
+
+    // Test for the arch_scan() method of the Scan struct
+    #[test]
+    fn test_scan_arch_scan() {
+        let scan = Scan::new();
+        let arch_pyresult = scan.arch_scan();
+        assert!(arch_pyresult.is_ok());
+        let arch_string = arch_pyresult.unwrap();
+        assert_eq!(arch_string, ARCH);
     }
 }
+
