e62ba40f687ef148e09052878790cc1ac09ac17d
chainyo <thomas@deeptools.ai>
2023-05-01 11:36:07 +0200

    add ModelConfig struct

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,6 @@
 use pyo3::prelude::*;
 use pyo3::exceptions::PyValueError;
+use pyo3::types::PyDict;
 
 use crate::error::ConfigError;
 
@@ -90,15 +91,73 @@ impl ModelConfig {
         }
     }
 
-    /// Returns a string representing the model config.
-    /// 
-    /// # Example
-    /// 
-    /// ```
-    /// use aiha::config::ModelConfig;
+    #[getter]
+    pub fn hidden_size(&self) -> PyResult<Option<i32>> {
+        Ok(self.hidden_size)
+    }
+
+    #[setter]
+    pub fn set_hidden_size(&mut self, value: Option<i32>) -> PyResult<()> {
+        self.hidden_size = value;
+        Ok(())
+    }
+
+    #[getter]
+    pub fn intermediate_size(&self) -> PyResult<Option<i32>> {
+        Ok(self.intermediate_size)
+    }
+
+    #[setter]
+    pub fn set_intermediate_size(&mut self, value: Option<i32>) -> PyResult<()> {
+        self.intermediate_size = value;
+        Ok(())
+    }
+
+    #[getter]
+    pub fn max_position_embeddings(&self) -> PyResult<Option<i32>> {
+        Ok(self.max_position_embeddings)
+    }
+
+    #[setter]
+    pub fn set_max_position_embeddings(&mut self, value: Option<i32>) -> PyResult<()> {
+        self.max_position_embeddings = value;
+        Ok(())
+    }
+
+    #[getter]
+    pub fn num_attention_heads(&self) -> PyResult<Option<i32>> {
+        Ok(self.num_attention_heads)
+    }
+
+    #[setter]
+    pub fn set_num_attention_heads(&mut self, value: Option<i32>) -> PyResult<()> {
+        self.num_attention_heads = value;
+        Ok(())
+    }
+
+    #[getter]
+    pub fn num_hidden_layers(&self) -> PyResult<Option<i32>> {
+        Ok(self.num_hidden_layers)
+    }
+
+    #[setter]
+    pub fn set_num_hidden_layers(&mut self, value: Option<i32>) -> PyResult<()> {
+        self.num_hidden_layers = value;
+        Ok(())
+    }
+
+    /// Returns a python dictionary containing the model config.
     #[pyo3(text_signature = "($self)")]
-    pub fn get_config(&self) -> String {
-        format!("{{\"hidden_size\":{},\"intermediate_size\":{},\"max_position_embeddings\":{},\"num_attention_heads\":{},\"num_hidden_layers\":{}}}", self.hidden_size.unwrap(), self.intermediate_size.unwrap(), self.max_position_embeddings.unwrap(), self.num_attention_heads.unwrap(), self.num_hidden_layers.unwrap())
+    pub fn to_dict(&self) -> Result<Py<PyDict>, pyo3::PyErr> {
+        Python::with_gil(|py| {
+            let dict = PyDict::new(py);
+            dict.set_item("hidden_size", self.hidden_size)?;
+            dict.set_item("intermediate_size", self.intermediate_size)?;
+            dict.set_item("max_position_embeddings", self.max_position_embeddings)?;
+            dict.set_item("num_attention_heads", self.num_attention_heads)?;
+            dict.set_item("num_hidden_layers", self.num_hidden_layers)?;
+            Ok(dict.into_py(py))
+        })
     }
 }
 
@@ -114,17 +173,36 @@ mod tests {
         assert_eq!(config_url.get_url(), "https://huggingface.co/EleutherAI/gpt-neox-20b/resolve/main/config.json");
     }
 
-    // Test for the ModelConfig struct
-    #[test]
-    fn test_model_config() {
-        let config = ModelConfig::new(Some(1024), Some(4096), Some(2048), Some(16), Some(24)).unwrap();
-        assert_eq!(config.get_config(), "{\"hidden_size\":1024,\"intermediate_size\":4096,\"max_position_embeddings\":2048,\"num_attention_heads\":16,\"num_hidden_layers\":24}");
-    }
-
     // Test for the ConfigError struct
     #[test]
     fn test_config_error() {
         let config = ModelConfig::new(None, None, None, None, None);
         assert_eq!(config.is_err(), true);
+    }
+
+    // Test for the ModelConfig struct
+    #[test]
+    fn test_model_config() {
+        let config = ModelConfig::new(Some(1024), Some(4096), Some(2048), Some(16), Some(32));
+        assert_eq!(config.is_ok(), true);
+    }
+
+    // Test for the ModelConfig struct
+    #[test]
+    fn test_to_dict() {
+        pyo3::prepare_freethreaded_python();
+        let config = ModelConfig::new(Some(1024), Some(4096), Some(2048), Some(16), Some(32)).unwrap();
+        let dict_result = config.to_dict();
+        assert!(dict_result.is_ok());
+        let dict = dict_result.unwrap();
+
+        Python::with_gil(|py| {
+            let dict_ref = dict.as_ref(py);
+            assert_eq!(dict_ref.get_item("hidden_size").unwrap().extract::<i32>().unwrap(), 1024);
+            assert_eq!(dict_ref.get_item("intermediate_size").unwrap().extract::<i32>().unwrap(), 4096);
+            assert_eq!(dict_ref.get_item("max_position_embeddings").unwrap().extract::<i32>().unwrap(), 2048);
+            assert_eq!(dict_ref.get_item("num_attention_heads").unwrap().extract::<i32>().unwrap(), 16);
+            assert_eq!(dict_ref.get_item("num_hidden_layers").unwrap().extract::<i32>().unwrap(), 32);
+        });
     }
 }
