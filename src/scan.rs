77ce971f1f06d334c6fe5f34cb619cbb4137c8d0
chainyo <thomas@deeptools.ai>
2023-05-01 13:37:53 +0200

    improve Scan class to be staticmethods only

--- src/scan.rs
+++ src/scan.rs
@@ -1,5 +1,6 @@
 use pyo3::prelude::*;
 use std::env::consts::{ARCH, OS};
+use std::fs;
 use std::path::PathBuf;
 
 #[pyclass]
@@ -7,35 +8,33 @@ pub struct Scan {}
 
 #[pymethods]
 impl Scan {
-    #[new]
-    pub fn new() -> Self {
-        Scan {}
-    }
     /// Returns a string representing the operating system the code is running on.
-    ///
-    /// # Examples
-    ///
+    /// 
+    /// # Example
+    /// 
     /// ```
-    /// let scanner = Scanner;
-    /// let os = scanner.os_scan();
-    /// println!("Operating system: {}", os);
+    /// use aiha::scan::Scan;
+    /// 
+    /// let os = Scan::os_scan();
+    /// println!("{}", os);
     /// ```
-    #[pyo3(text_signature = "($self)")]
-    pub fn os_scan(&self) -> PyResult<String> {
+    #[staticmethod]
+    pub fn os_scan() -> PyResult<String> {
         Ok(OS.to_string())
     }
 
     /// Returns a string representing the CPU architecture the code is running on.
-    ///
-    /// # Examples
-    ///
+    /// 
+    /// # Example
+    /// 
     /// ```
-    /// let scanner = Scanner;
-    /// let arch = scanner.arch_scan();
-    /// println!("CPU architecture: {}", arch);
+    /// use aiha::scan::Scan;
+    /// 
+    /// let arch = Scan::arch_scan();
+    /// println!("{}", arch);
     /// ```
-    #[pyo3(text_signature = "($self)")]
-    pub fn arch_scan(&self) -> PyResult<String> {
+    #[staticmethod]
+    pub fn arch_scan() -> PyResult<String> {
         Ok(ARCH.to_string())
     }
 
@@ -56,12 +55,102 @@ impl Scan {
     }
 
     /// Returns a string representing the user's home directory.
-    #[pyo3(text_signature = "($self)")]
-    pub fn home_dir(&self) -> PyResult<Option<String>> {
+    #[staticmethod]
+    pub fn home_dir() -> PyResult<Option<String>> {
         match Scan::get_home_dir() {
             Some(path) => Ok(Some(path.to_str().unwrap().to_string())),
             None => Ok(None),
         }
+    }
+
+    /// Create the AIHA cache folder in the user's home directory if it doesn't exist.
+    /// 
+    /// # Example
+    /// 
+    /// ```
+    /// use aiha::scan::Scan;
+    /// 
+    /// Scan::create_aiha_cache_folder();
+    /// ```
+    #[staticmethod]
+    fn create_aiha_cache_folder() -> std::io::Result<()> {
+        // Create the AIHA cache folder in the user's home directory if it doesn't exist
+        if let Some(mut cache_dir) = Scan::get_home_dir() {
+            cache_dir.push(".cache");
+            cache_dir.push("aiha");
+
+            if !cache_dir.exists() {
+                fs::create_dir_all(&cache_dir)?;
+            }
+        } else {
+            eprintln!("Failed to get home directory");
+        }
+        Ok(())
+    }
+
+    /// Return the path to the AIHA cache folder in the user's home directory.
+    /// If the folder doesn't exist, create it.
+    /// 
+    /// # Example
+    /// 
+    /// ```
+    /// use aiha::scan::Scan;
+    /// 
+    /// let cache_dir = Scan::get_aiha_cache_folder();
+    /// println!("{}", cache_dir);
+    /// ```
+    #[staticmethod]
+    fn get_aiha_cache_folder() -> Option<PathBuf> {
+        if let Err(e) = Scan::create_aiha_cache_folder() {
+            eprintln!("Failed to create AIHA cache folder: {}", e);
+        }
+
+        if let Some(mut cache_dir) = Scan::get_home_dir() {
+            cache_dir.push(".cache");
+            cache_dir.push("aiha");
+            Some(cache_dir)
+        } else {
+            None
+        }
+    }
+
+    /// Create the folders for one model in the AIHA cache folder.
+    #[staticmethod]
+    pub fn create_model_cache_folder(model_name: &str) -> std::io::Result<()> {
+        // If model_name contains a / then we need to create an organization folder
+        // and a model folder, otherwise we just need to create a model folder
+        if model_name.contains("/") {
+            let mut model_path = Scan::get_aiha_cache_folder().unwrap();
+            let model_name_split: Vec<&str> = model_name.split("/").collect();
+            let org_name = model_name_split[0];
+            let model_name = model_name_split[1];
+
+            model_path.push(org_name);
+            model_path.push(model_name);
+
+            if !model_path.exists() {
+                fs::create_dir_all(&model_path)?;
+            }
+        } else {
+            let mut model_path = Scan::get_aiha_cache_folder().unwrap();
+            model_path.push(model_name);
+
+            if !model_path.exists() {
+                fs::create_dir_all(&model_path)?;
+            }
+        }
+        Ok(())
+    }
+
+    /// Clean the AIHA cache folder.
+    #[staticmethod]
+    pub fn clean_cache() -> std::io::Result<()> {
+        if let Some(cache_dir) = Scan::get_aiha_cache_folder() {
+            fs::remove_dir_all(&cache_dir)?;
+        } else {
+            eprintln!("Failed to get AIHA cache folder");
+        }
+        Ok(())
     }
 }
 
@@ -69,39 +158,69 @@ impl Scan {
 mod tests {
     use super::*;
 
-    // Test for the new() method of the Scan struct
-    #[test]
-    fn test_scan_new() {
-        let scan = Scan::new();
-        assert!(scan.os_scan().is_ok());
-        assert!(scan.arch_scan().is_ok());
-    }
-
     // Test for the os_scan() method of the Scan struct
     #[test]
-    fn test_scan_os_scan() {
-        let scan = Scan::new();
-        let os_pyresult = scan.os_scan();
-        assert!(os_pyresult.is_ok());
-        let os_string = os_pyresult.unwrap();
-        assert_eq!(os_string, OS);
+    fn test_os_scan() {
+        let os = Scan::os_scan();
+        assert!(os.is_ok());
     }
 
     // Test for the arch_scan() method of the Scan struct
     #[test]
-    fn test_scan_arch_scan() {
-        let scan = Scan::new();
-        let arch_pyresult = scan.arch_scan();
-        assert!(arch_pyresult.is_ok());
-        let arch_string = arch_pyresult.unwrap();
-        assert_eq!(arch_string, ARCH);
+    fn test_arch_scan() {
+        let arch = Scan::arch_scan();
+        assert!(arch.is_ok());
+    }
+
+    // Test for the home_dir() method of the Scan struct
+    #[test]
+    fn test_home_dir() {
+        let home_dir = Scan::home_dir();
+        assert!(home_dir.is_ok());
+    }
+
+    // Test for the get_aiha_cache_folder() method of the Scan struct
+    #[test]
+    fn test_get_aiha_cache_folder() {
+        let cache_dir = Scan::get_aiha_cache_folder();
+        assert!(cache_dir.is_some());
+    }
+
+    // Test for the create_aiha_cache_folder() method of the Scan struct
+    #[test]
+    fn test_create_aiha_cache_folder() {
+        let cache_dir = Scan::create_aiha_cache_folder();
+        assert!(cache_dir.is_ok());
     }
 
     // Test for the get_home_dir() method of the Scan struct
     #[test]
-    fn test_scan_get_home_dir() {
+    fn test_get_home_dir() {
         let home_dir = Scan::get_home_dir();
         assert!(home_dir.is_some());
+    }
+
+    // Test for the create_model_cache_folder() method of the Scan struct
+    #[test]
+    fn test_create_model_cache_folder_no_org() {
+        let model_name = "test_model";
+        let model_cache_folder = Scan::create_model_cache_folder(model_name);
+        assert!(model_cache_folder.is_ok());
+    }
+
+    // Test for the create_model_cache_folder() method of the Scan struct
+    #[test]
+    fn test_create_model_cache_folder_with_org() {
+        let model_name = "test_org/test_model";
+        let model_cache_folder = Scan::create_model_cache_folder(model_name);
+        assert!(model_cache_folder.is_ok());
+    }
+
+    // Test for the clean_cache() method of the Scan struct
+    #[test]
+    fn test_clean_cache() {
+        let clean_cache = Scan::clean_cache();
+        assert!(clean_cache.is_ok());
     }
 }
 
