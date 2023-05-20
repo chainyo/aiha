d606e95bb7684958da63ea166beb8ff077bd13d8
chainyo <thomas@deeptools.ai>
2023-05-20 12:32:39 +0200

    update lib desc and imports

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,13 @@
+#![deny(missing_docs)]
+//! # AIHA - AI Hardware Advisor
+//! 
+//! Introducing the **AI Hardware Advisor (AIHA)** – Unlock the Power of Optimal Hardware for **Your AI Project**!
+//! 
+//! Looking to embark on an AI journey? Look no further! **AI Hardware Advisor (AIHA)** is the ultimate tool that empowers
+//! you to make intelligent decisions when selecting hardware for your AI endeavors.
+//! 
+//! With **AIHA**, the guessing game is over. Say goodbye to uncertainty and welcome a world of precise resource allocation
+//! for inference and training any model on the esteemed Hugging Face Hub.
+//!
 pub mod hardware;
+pub use hardware::{ scan_hardware, Hardware, NvidiaDevice };
